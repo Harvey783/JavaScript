d49768aba04bdf03f659982d37fa929fafe4d106
Christopher Harvey <chharvey@icloud>
2019-08-15 08:51:41 -0400

    chunk

--- Ex/chunk/index.js
+++ Ex/chunk/index.js
@@ -8,21 +8,36 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  // Create an empty array to hold chunks
-  const chunked = [];
+// function chunk(array, size) {
+//   // Create an empty array to hold chunks
+//   const chunked = [];
 
-  for (let element of array) {
-    // For each element in the array retrieve the last element
-    const last = chunked[chunked.length - 1];
-    //  If the last element does not exist or its length is equal to chunk size
-    if (!last || last.length === size) {
-      // Push a new chunk into 'chunked' with the current element
-      chunked.push([element]);
-    } else {
-      // Add the current element into the chunk
-      last.push(element);
-    }
+//   for (let element of array) {
+//     // For each element in the array retrieve the last element
+//     const last = chunked[chunked.length - 1];
+//     //  If the last element does not exist or its length is equal to chunk size
+//     if (!last || last.length === size) {
+//       // Push a new chunk into 'chunked' with the current element
+//       chunked.push([element]);
+//     } else {
+//       // Add the current element into the chunk
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+function chunk(array, size) {
+  // Create an empty 'chunked' array
+  const chunked = [];
+  // Create 'index' starting at 0
+  let index = 0;
+  // While index is less than array.length
+  while (index < array.length) {
+    // Push a slice of length 'size' from 'array' into 'clunked'
+    chunked.push(array.slice(index, index + size));
+    // Add 'slice' to 'index'
+    index += size;
   }
   return chunked;
 }
