a6ae49aae62a1ed38c06d296d71d7ee3047024b1
Christopher Harvey <chharvey@icloud>
2019-08-15 08:44:09 -0400

    chunk

--- Ex/chunk/index.js
+++ Ex/chunk/index.js
@@ -8,6 +8,23 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // Create an empty array to hold chunks
+  const chunked = [];
+
+  for (let element of array) {
+    // For each element in the array retrieve the last element
+    const last = chunked[chunked.length - 1];
+    //  If the last element does not exist or its length is equal to chunk size
+    if (!last || last.length === size) {
+      // Push a new chunk into 'chunked' with the current element
+      chunked.push([element]);
+    } else {
+      // Add the current element into the chunk
+      last.push(element);
+    }
+  }
+  return chunked;
+}
 
 module.exports = chunk;
